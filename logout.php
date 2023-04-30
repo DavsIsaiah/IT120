8eef7800c2d236f55324a2927fb60605d55d1089
“DavsIsaiah” <jidava@mymail.mapua.edu.ph>
2023-04-30 12:21:40 +0800

    Fixes

--- logout.php
+++ logout.php
@@ -2,8 +2,9 @@
 session_start();
 if (isset($_SESSION["user"])) {
     session_destroy();
-    echo '<script>alert("Successfully Logged Out.");</script>';
-    header("location:login.php");
+    echo '<script>alert("Successfully Logged Out.");
+    window.location = "login.php";</script>';
+
 } else {
     session_destroy();
     echo '<script>alert("You are not logged in!");
